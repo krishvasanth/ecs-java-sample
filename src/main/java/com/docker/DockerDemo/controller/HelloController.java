7f394c90dcc8378d8faca6c3c671a3b14bde837d
Radhakrishnan C <radhakrishnan.c@jothisoftware.com>
2021-01-05 17:12:27 +0530

    update code

--- src/main/java/com/docker/DockerDemo/controller/HelloController.java
+++ src/main/java/com/docker/DockerDemo/controller/HelloController.java
@@ -12,7 +12,7 @@ public class HelloController {
 
 	@RequestMapping(path = "/greeting", method = RequestMethod.GET)
 	public Greeting sayHello(@RequestParam String name) {
-		String message = "Welcome to Amazon Its time we change " + name;
+		String message = "Hello World !!!. Name " + name;
 		Greeting dto = new Greeting();
 		dto.setMessage(message);
 		return dto;
